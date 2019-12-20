be212bfce4bdd6b94e8c95d0e5d5e9f8702c6cfe
Marcel Lauhoff <ml@irq0.org>
2019-12-20 21:38:48 +0100

    upgrade dependencies

--- project.clj
+++ project.clj
@@ -11,22 +11,21 @@
   :main cathica.core
 
   :dependencies [
-                 [org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojure "1.10.1"]
                  [com.taoensso/timbre "4.10.0"]
-                 [org.slf4j/log4j-over-slf4j "1.7.25"]
-                 [org.slf4j/jul-to-slf4j "1.7.26"]
-                 [org.slf4j/jcl-over-slf4j "1.7.26"]
-                 [com.fzakaria/slf4j-timbre "0.3.12"]
+                 [org.slf4j/log4j-over-slf4j "1.7.30"]
+                 [org.slf4j/jul-to-slf4j "1.7.30"]
+                 [org.slf4j/jcl-over-slf4j "1.7.30"]
+                 [com.fzakaria/slf4j-timbre "0.3.17"]
                  [mount "0.1.16"]
-                 [clj-time "0.15.1"]
+                 [clj-time "0.15.2"]
                  [nrepl "0.6.0"]
-                 [cider/cider-nrepl "0.21.1"]
-                 [org.clojure/core.async "0.4.490"]
+                 [cider/cider-nrepl "0.22.4"]
+                 [org.clojure/core.async "0.6.532"]
                  [slingshot "0.12.2"]
-                 [com.novemberain/pantomime "2.10.0"]
+                 [com.novemberain/pantomime "2.11.0"]
                  ;; for dbus-java
                  [com.github.hypfvieh/libmatthew "0.8.3"]
                  [robert/hooke "1.3.0"]
                  [seesaw "1.5.0"]
-                 [org.jfxtras/jfxtras-all "10.0-r1"]
-                 ])
+                 [org.jfxtras/jfxtras-all "10.0-r1"]])
