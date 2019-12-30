f66502d6093c2f0db1e51e6fe123c4dde5b9b8b8
Marcel Lauhoff <ml@irq0.org>
2019-12-30 11:26:47 +0100

    rules: better web search

--- src/cathica/rules.clj
+++ src/cathica/rules.clj
@@ -9,16 +9,24 @@
    [clj-time.local :as tl]
    [clojure.java.io :as io]
    [clojure.java.shell :refer [sh]]
+   [clj-http.client :as http]
    [clojure.string :as string]
+   [byte-streams :as bytestreams]
+   [pantomime.mime :as pantomime]
    [taoensso.timbre :as log]
    [slingshot.slingshot :refer [throw+ try+]]
    [mount.core :as mount :refer [defstate]]
    [clojure.edn :as edn]))
 
+(def 🖖-bookmark-url "http://10.23.1.23:8023/reader/bookmark/add")
+(def 🖖-bookmark-item-base-url "http://10.23.1.23:8023/reader/group/type/bookmark/source/all/item/by-id/")
+
 (def terminal-re #"urxvt256c|Alacritty|main")
 (def web-browser-re #"Navigator|google-chrome")
 (def bugtracker-base-url "https://mantis/view.php?id=")
 (def gerrit-change-base-url "https://gerrit/#/q/")
+
+(def url-regex #"(?U)(https?|ftp)://[a-zA-Z0-9_@\-]+([.:][a-zA-Z0-9_@\-]+)*/?[a-zA-Z0-9_?,%#~&/\-+=]+([:.][a-zA-Z0-9_?,%#~&/\-+=]+)*")
 
 (defonce selection (.getSystemSelection (java.awt.Toolkit/getDefaultToolkit)))
 
@@ -46,90 +54,167 @@
   ([file]
    (sh "emacsclient" "--no-wait" file)))
 
+(defn youtube-dl-video [url]
+  (let [{:keys [exit out err]}
+        (sh
+         "youtube-dl"
+         "--format" "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"
+         "--recode-video" "mp4"
+         "--embed-subs"
+         url
+         :dir
+         (str (System/getProperty "user.home") "/Desktop"))
+        youtube-err (second (re-find #"YouTube said:\s+(.+)$" err))]
+    (log/info "Youtube-dl OUT: " out)
+    (log/debug "Youtube-dl ERR: " err)
+    (if (zero? exit)
+      (let [[_ out-filename] (re-find #"Destination:\s(.+\.mp4)" out)]
+        out-filename)
+      (do
+        (log/error "Youtube-dl error: " youtube-err)
+        (desktop-notification "Youtube-dl error" youtube-err)))))
 
-(def +default-rules+
-  [(rule
-     {:name "Google Calendar"
-      :type :text
-      :data #"(\d{4})-(\d{2})-(\d{2})T\d{2}:\d{2}:\d{2}Z?"
-      :start (browse (format "https://calendar.google.com/calendar/r/week/%s/%s/%s" $1 $2 $3))})
-   (rule
-     {:name "Search ISBN on Amazon"
-      :type :text
-      :data #"([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])'"
-      :start (browse (str "http://www.amazon.com/s/?field-keywords=" $1))})
-   (rule
-     {:name "Jenkins Build"
-      :type :text
-      :data #"Build[ ]*#([0-9]+)"
-      :start (browse (str "https://jenkins/job/gerrit_review_build/" $1))})
-   (rule
-     {:name "Manits Issue"
-      :type :text
-      :data #"#([0-9]+)"
-      :start (browse (str "https://mantis/view.php?id=" $1))})
-   (rule
-     {:name "Browse URL"
-      :type :text
-      :data #"(https?|ftp)://[a-zA-Z0-9_@\-]+([.:][a-zA-Z0-9_@\-]+)*/?[a-zA-Z0-9_?,%#~&/\-+=]+([:.][a-zA-Z0-9_?,%#~&/\-+=]+)*"
-      :start (browse $0)})
-   (rule
-     {:name "Gerrit Change"
-      :type :text
-      :data #"Change-Id: ([a-zA-Z0-9]+)"
-      :start (browse (str "https://gerrit/#/q/" $1))})
-   (rule
-     {:name "Browse local html"
-      :type :text
-      :data #"(file://)?([a-zA-Z0-9_\-\: \/]+\.html?|htm)"
-      :arg (is-file $2)
-      :start (browse $arg)})
-   (rule
-     {:name "Open java file matching stacktrace line"
-      :type :text
-      :data #"([a-zA-Z0-9]+\.)+[a-zA-Z0-9]+\([a-zA-Z0-9]+\.java\:[0-9]+\)"
-      :start (sh "open_by_stacktrace_line java" $0)})
-   (rule
-     {:name "Image viewer"
-      :type :text
-      :data #"(file://)?(\p{Print}+\.(png|PNG|jpe?g|JPE?G|gif|GIF|tiff?|TIFF?))"
-      :arg (is-file $2)
-      :start (sh "feh" $arg)})
-   (rule
-     {:name "Gmail - msgid"
-      :type :text
-      :data #"rfc822msgid:(.+)|msgid://(.+)"
-      :start (browse (format "https://mail.google.com/#search/rfc822msgid:%s" $1))})
-   (rule
-     {:name "Gmail - Write Email"
-      :type :text
-      :data #"[a-zA-Z0-9\._%+\-]+@[a-zA-Z0-9\.\-]+\.[a-zA-Z]+"
-      :start (sh "gmail" $0)})
-   (rule
-     {:name "Gmail - Write Email"
-      :type :text
-      :data #".*[a-zA-Z0-9 \-_]+ \<([a-zA-Z0-9\._%+\-]+@[a-zA-Z0-9\.\-]+\.[a-zA-Z]+)\>.*"
-      :start (sh "gmail" $0)})
-   (rule
-    {:name "Open Log Filename in Emacs"
-     :type :text
+
+(def query-string-re #"(?U)([\w ]+)")
+
+(def web-search-engines
+  [["Amazon" #(str "http://www.amazon.de/exec/obidos/external-search?keyword=" %)]
+   ["Google" #(str "https://www.google.com/complete/search?client=firefox&q=" %)]
+   ["Reddit" #(str "https://www.reddit.com/search?q=" %)]
+   ["GitHub" #(str "https://github.com/search?ref=opensearch&q=" %)]
+   ["Google Maps" #(str "https://www.google.com/maps/search/" % "?hl=en&source=opensearch")]
+   ["Wikipedia" #(str "https://en.wikipedia.org/w/index.php?sort=relevance&search=" %
+                      "&title=Special:Search&profile=advanced&fulltext=1")]
+   ["Cppreference" #(str "https://en.cppreference.com/mwiki/index.php?search=" %)]
+   ["Ebay" #(str "https://www.ebay.de/sch/i.html?_nkw=" %)]
+   ["DevDocs" #(str "https://devdocs.io/#q=" %)]])
+
+(def +web-search-rules+
+  (for [[query-engine-name url-fn] web-search-engines]
+    (rule (str "Search " query-engine-name)
+     {:type :text
+      :data query-string-re
+      :start (browse (url-fn(java.net.URLEncoder/encode $1 "UTF-8")))})))
+
+
+(def logistics-tracing
+  [["DHL" #"([0-9]{10,11})" #(str "https://www.dhl.de/en/privatkunden/"
+                                  "pakete-empfangen/verfolgen.html?piececode=" %)]
+   ["17track" #"(\w+)" #(str "https://t.17track.net/de#nums=" %)]])
+
+(def +logistics-tracing+
+  (for [[query-engine-name regex url-fn] logistics-tracing]
+    (rule (str "Track and Trace: " query-engine-name)
+     {:type :text
+      :data regex
+      :start (browse (url-fn(java.net.URLEncoder/encode $1 "UTF-8")))})))
+
+
+(def +specialized-search-rules+
+  [(rule "Manits Issue"
+    {:type :text
+     :data #"#([0-9]+)"
+     :start (browse (str bugtracker-base-url $1))})
+   (rule "Gerrit Change"
+    {:type :text
+     :data #"Change-Id: ([a-zA-Z0-9]+)"
+     :start (browse (str gerrit-change-base-url $1))})
+   (rule "c++: search cppreference for std::"
+    {:type :text
+     :data #"std::[a-zA-Z]+"
+     :start (browse (str "http://en.cppreference.com/mwiki/index.php?title=Special%3ASearch&search=" $0))})
+   (rule "Browse RFC"
+    {:type :text
+     :data #"(RFC|rfc)[ ]*([0-9]+)"
+     :start (browse (str "https://www.rfc-editor.org/rfc/rfc" $2 ".txt"))})
+   (rule "Search Dictionary"
+    {:type :text
+     :data #"(?U).+"
+     :start (sh "search_dict" (string/trim $0))})
+   (rule "Search Web"
+    {:type :text
+     :data #"(?U).+"
+     :start (sh "search_web" (string/trim $0))})])
+
+(def +rules+
+  [(rule "Google Calendar"
+    {:type :text
+     :data #"(\d{4})-(\d{2})-(\d{2})T\d{2}:\d{2}:\d{2}Z?"
+     :start (browse (format "https://calendar.google.com/calendar/r/week/%s/%s/%s" $1 $2 $3))})
+   (rule "Search ISBN on Amazon"
+    {:type :text
+     :data #"([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])'"
+     :start (browse (str "http://www.amazon.com/s/?field-keywords=" $1))})
+   (rule "Browse URL"
+    {:type :text
+     :data url-regex
+     :start (browse $0)})
+   (rule "Browse local html"
+    {:type :text
+     :data #"(?U)(file://)?([a-zA-Z0-9_\-\: \/]+\.html?|htm)"
+     :arg (is-file $2)
+     :start (browse $arg)})
+   (rule "Open java file matching stacktrace line"
+    {:type :text
+     :data #"([a-zA-Z0-9]+\.)+[a-zA-Z0-9]+\([a-zA-Z0-9]+\.java\:[0-9]+\)"
+     :start (sh "open_by_stacktrace_line java" $0)})
+   (rule "Image viewer"
+    {:type :text
+     :data #"(?U)(file://)?(\p{Print}+\.(png|PNG|jpe?g|JPE?G|gif|GIF|tiff?|TIFF?))"
+     :arg (is-file $2)
+     :start (sh "feh" $arg)})
+   (rule "Image viewer"
+    {:type :image
+     :start (let [extension (pantomime/extension-for-name $TYPE)
+                  tmp-file (java.io.File/createTempFile "cathica-image-" extension)]
+              (bytestreams/transfer $DATA tmp-file)
+              (sh "feh" (.getAbsolutePath tmp-file))
+              (.deleteOnExit tmp-file))})
+   (rule "Document viewer"
+    {:type :text
+     :data #"(?U)(file://)?(\p{Print}+\.(pdf))"
+     :arg (is-file $2)
+     :start (sh "mupdf" $arg)})
+   (rule "Video Player: Youtube Link"
+     {:type :text
+      :data #"(?:http(?:s?):\/\/)?(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?"
+      :start (sh "mpv" $0)})
+   (rule "Video Player: Download in Background"
+     {:type :text
+      :data #"(?:http(?:s?):\/\/)?(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?"
+      :start (future
+               (desktop-notification "Starting youtube download" $1)
+               (let [filename (youtube-dl-video
+                               $1)]
+                 (desktop-notification "Download finished" filename)))})
+   (rule "Gmail - msgid"
+    {:type :text
+     :data #"rfc822msgid:(.+)|msgid://(.+)"
+     :start (browse (format "https://mail.google.com/#search/rfc822msgid:%s" $1))})
+   (rule "Gmail - Write Email"
+    {:type :text
+     :data #"[a-zA-Z0-9\._%+\-]+@[a-zA-Z0-9\.\-]+\.[a-zA-Z]+"
+     :start (sh "gmail" $0)})
+   (rule "Gmail - Write Email"
+    {:type :text
+     :data #".*[a-zA-Z0-9 \-_]+ \<([a-zA-Z0-9\._%+\-]+@[a-zA-Z0-9\.\-]+\.[a-zA-Z]+)\>.*"
+     :start (sh "gmail" $0)})
+   (rule "Open Mangeled Log Filename in Emacs"
+    {:type :text
      :data #"…([\w_\/]+\.\w+):(\d+)"
      :src terminal-re
      :arg (git-resolve-file $1)
      :start (emacs-open $arg [(str "+" $2 ":" 0)])})
-
-   (rule
-    {:name "Open Compile Error in Emacs"
-     :type :text
+   (rule "Open Compile Error in Emacs"
+    {:type :text
      :data #"^../([a-zA-Z0-9_\-\:\. \(\)\/]+):(\d+):(\d+):\s+(\w+):\s+(.+)"
      :src terminal-re
      :arg (is-file $1)
      :start (do
               (desktop-notification (str "Compiler " $4 " in " $1 " line " $2) $5 :expire-time 0)
               (emacs-open $arg [(str "+" $2 ":" $3)]))})
-   (rule
-    {:name "Show timestamp range"
-     :type :text
+   (rule "Show timestamp range"
+    {:type :text
      :data #"(?s).+_timestamp_ms: (\d+)\n.+_timestamp_ms: (\d+)"
      :start (let [start (tc/from-long (Long/parseLong $1))
                   end (tc/from-long (Long/parseLong $2))
@@ -150,10 +235,8 @@
                                      "Duration: "
                                      (format-interval (time/interval start end)))
                                     :expire-time 0))})
-
-   (rule
-    {:name "Convert Qtimestamps"
-     :type :text
+   (rule "Convert Qtimestamps"
+    {:type :text
      :data #".*(timestamp|time)_(ms|s): (\d+)"
      :start (let [unit-multiplier (case $2
                                     "ms" 1
@@ -162,14 +245,15 @@
                   show-ts ["UTC" "Europe/Berlin" "US/Pacific" "US/Central" "US/Eastern"]
                   msg (string/join "\n"
                                    (map #(str % ": "
-                                              (tl/format-local-time (time/to-time-zone ts (time/time-zone-for-id %)) :mysql))
+                                              (tl/format-local-time
+                                               (time/to-time-zone
+                                                ts
+                                                (time/time-zone-for-id %)) :mysql))
                                         show-ts))]
               (desktop-notification (str "ts: " $0) msg :expire-time 0)
               (set-clipboard-string selection msg))})
-
-   (rule
-    {:name "Convert Duration (to clipboard)"
-     :type :text
+   (rule "Convert Duration (to clipboard)"
+    {:type :text
      :data #"(\d+)(ms|ns|s)"
      :start (let [unit-multiplier (case $2
                                     "ms" 1
@@ -181,47 +265,59 @@
                        (org.joda.time.Period. (long millis)))]
               (set-clipboard-string selection msg)
               (desktop-notification (str "duration: " msg)  :expire-time 0))})
-
-   (rule
-    {:name "Browse svg"
-     :type :text
+   (rule "Browse svg"
+    {:type :text
      :data #"(file://)?([a-zA-Z0-9_\-\: \/]+\.svg|SVG)"
      :arg (is-file $2)
      :start (browse $arg)})
-   (rule
-    {:name "dired"
-     :type :text
+   (rule "Emacs: dired"
+    {:type :text
      :data #".*"
      :arg (is-dir $0)
      :start (emacs-open $arg)})
-   (rule
-    {:name "emacs edit"
-     :type :text
+   (rule "Emacs: edit"
+    {:type :text
      :data #"(file://)?([a-zA-Z0-9_\-\:\. \(\)\/]+)"
      :arg (is-file $2)
      :start (emacs-open $arg)})
-   (rule
-    {:name "commit in magit"
-     :type :text
+   (rule "Emacs: Commit in Magit"
+    {:type :text
      :data #"[0-9a-f]{5,}"
-     :start (sh "open_magit_commit" $0)})
+     :start (sh "open_magit_commit" $0)
+     })])
+
+
+(defn 🖖-bookmark [type url]
+  (future
+    (let [resp (http/post 🖖-bookmark-url
+                          {:content-type :json
+                           :form-params {:url url
+                                         :type type}})
+          item-id (:body resp)
+          🖖-url (str 🖖-bookmark-item-base-url item-id)]
+
+      (desktop-notification "bookmark Ready"
+                            🖖-url)
+      (browse 🖖-url))))
+
+(def +🖖-rules+
+  [(rule
+    "🖖 Add Raw Bookmark"
+    {:type :text
+     :data url-regex
+     :start (🖖-bookmark :raw-bookmark $0)})
    (rule
-    {:name "c++: search cppreference for std::"
-     :type :text
-     :data #"std::[a-zA-Z]+"
-     :start (browse (str "http://en.cppreference.com/mwiki/index.php?title=Special%3ASearch&search=" $0))})
-   (rule
-    {:name "Browse RFC"
-     :type :text
-     :data #"(RFC|rfc)[ ]*([0-9]+)"
-     :start (browse (str "https://www.rfc-editor.org/rfc/rfc" $2 ".txt"))})
-   (rule
-    {:name "Search dict"
-     :type :text
-     :data #"([a-zA-Z]+)"
-     :start (sh "search_dict" $1)})
-   (rule
-    {:name "Search web"
-     :type :text
-     :data #"([a-zA-Z ]+)"
-     :start (sh "search_web" $1)})])
+    "🖖 Add Readability Bookmark"
+    {:type :text
+     :data url-regex
+     :start (🖖-bookmark :readability-bookmark $0)})
+   ])
+
+
+(def +default-rules+
+  (concat
+   +rules+
+   +specialized-search-rules+
+   +web-search-rules+
+   +🖖-rules+
+   +logistics-tracing+))
