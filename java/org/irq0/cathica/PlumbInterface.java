9715afdc5343bcb00679fdbae42bb0ac253806da
Marcel Lauhoff <ml@irq0.org>
2019-12-30 11:25:24 +0100

    add default action listener

--- java/org/irq0/cathica/PlumbInterface.java
+++ java/org/irq0/cathica/PlumbInterface.java
@@ -5,5 +5,7 @@ import org.freedesktop.dbus.DBusInterface;
 public interface PlumbInterface extends DBusInterface
 {
   public void clipboard();
+  public void clipboard_default_action();
   public void string(String str);
+  public void string_default_action(String str);
 }
